f19e2aa448dff5bdf449dc26909ead31774b92e5
XiaoYiHou <houxyi@cn.ibm.com>
2019-03-16 20:31:31 +0800

    controller包是在com.example.demo下面的，不可以是其他地方

--- src/main/java/com/example/demo/contrller/HelloWorldController.java
+++ src/main/java/com/example/demo/contrller/HelloWorldController.java
@@ -3,10 +3,13 @@ package com.example.demo.contrller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//如果是页面开发需要使用Controller，如果是返回数据json那么可以使用RestController
+
 @RestController
 public class HelloWorldController {
-    @RequestMapping("/hello")
-    public String index() {
-        return "Hello World";
-    }
+	@RequestMapping("/hello")
+	public String index() {
+		return "Hello World";
+	}
+
 }
