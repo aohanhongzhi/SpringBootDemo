36be024d4f20c1b6a0d3ef086150a82e54f649f9
aohanhongzhi <aohanhongzhi@qq.com>
2019-07-13 09:28:05 +0800

    删除多余代码

--- src/main/java/com/example/demo/contrller/HelloWorldController.java
+++ src/main/java/com/example/demo/contrller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.example.demo.contrller;
 
+import java.util.HashMap;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,6 +12,14 @@ public class HelloWorldController {
 	@RequestMapping("/hello")
 	public String index() {
 		return "Hello World";
+	}
+
+	@RequestMapping("/hellomap")
+	public HashMap<String, Object> map() {
+		HashMap<String, Object> hashMap = new HashMap<String, Object>();
+		hashMap.put("status", 200);
+		hashMap.put("result", "ok");
+		return hashMap;
 	}
 
 }
